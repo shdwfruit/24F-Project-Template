83f86e72fb406d15da7d77ef296df5e6dd6141b2
ArnavMehta11 <arnav.subscriptions11@gmail.com>
2024-12-04 23:13:43 -0500

    Creating the page configuration and title

---
+++ app/src/pages/Decision_Maker_Home.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+
+# Set page configuration
+st.set_page_config(page_title="Decision Maker Dashboard", layout="wide")
+
+# Page Title
+st.title("Decision Maker Dashboard")
+st.write("**Welcome, Dr. Smith!**")
+st.divider()
