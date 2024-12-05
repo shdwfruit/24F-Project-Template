5bf80ba48ce6e28b1cc92520e71cab9d8f5b7ca7
ArnavMehta11 <arnav.subscriptions11@gmail.com>
2024-12-04 23:25:15 -0500

    Model usage and completion data route

--- api/backend/system_admin/system_admin_routes.py
+++ api/backend/system_admin/system_admin_routes.py
@@ -5,3 +5,25 @@ from backend.db_connection import db
 # system administrator blueprint
 # Contains routes for system administrator functionalities
 sys_admin = Blueprint('system_admin', __name__)
+
+#------------------------------------------------------------
+# system administrator routes
+
+# Route: View module usage and completion data
+@sys_admin.route('/analytics/engagement', methods=['GET'])
+def get_module_engagement():
+    query = '''
+        SELECT path_id, COUNT(*) AS completions
+        FROM progress
+        WHERE status = 'Completed'
+        GROUP BY path_id
+    '''
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    theData = cursor.fetchall()
+
+    response = make_response(jsonify(theData))
+    response.status_code = 200
+    return response
+
+    return response
