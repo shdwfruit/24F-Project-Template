d38a2ffbda1ea45b68e3948702175f86962600e5
1214sean <seanhuang922922@gmail.com>
2024-12-05 16:17:00 -0500

    update session route

--- api/backend/sessions/session_routes.py
+++ api/backend/sessions/session_routes.py
@@ -11,3 +11,23 @@ from backend.db_connection import db
 sessions = Blueprint('session', __name__)
 
 #------------------------------------------------------------
+# Get sessions for mentors and mentees
+@sessions.route('/sessions', methods=['GET'])
+def get_sessions(user_id):
+    """
+    This route is used by mentors/mentees to retrieve their sessions.
+    """
+
+    query = f'''select *
+                from session
+                where mentor_id = {str(user_id)}
+                    or mentee_id = {str(user_id)}
+    '''
+
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    theData = cursor.fetchall()
+
+    response = make_response(jsonify(theData))
+    response.status_code = 200
+    return response
