c84d64b68ce3f36ce9927ef0b0a7379ad222a570
ArnavMehta11 <arnav.subscriptions11@gmail.com>
2024-12-04 22:19:13 -0500

    Update Admin_home.py
    Set up configuration and title

---
+++ app/src/pages/Admin_home.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+# Set page configuration
+st.set_page_config(page_title="System Administrator Dashboard", layout="wide")
+
+# Page Title
+st.title("System Administrator Dashboard")
+st.write("Welcome, Priya! Choose an action below:")
+
