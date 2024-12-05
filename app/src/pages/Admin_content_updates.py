fa07e1622fc6d9db7ee116a871e39f1f12d402c4
ArnavMehta11 <arnav.subscriptions11@gmail.com>
2024-12-04 22:37:39 -0500

    Connect the page to the MySQL

--- app/src/pages/Admin_content_updates.py
+++ app/src/pages/Admin_content_updates.py
@@ -4,3 +4,14 @@ import streamlit as st
 def content_updates_page():
     st.title("Manage Content Updates")
     st.divider()
+
+    # Connect to the MySQL
+    def connect_to_db():
+        from flaskext.mysql import MySQL
+        from pymysql import cursors
+        
+        # Initialize MySQL connection
+        mysql = MySQL(cursorclass=cursors.DictCursor)
+        app = {}
+        mysql.init_app(app)
+        return mysql.get_db()
