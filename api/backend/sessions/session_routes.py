bb11b4f5c46297f1ab8efae3a768d46f1536e925
1214sean <seanhuang922922@gmail.com>
2024-12-05 16:19:07 -0500

    update session route

--- api/backend/sessions/session_routes.py
+++ api/backend/sessions/session_routes.py
@@ -31,3 +31,24 @@ def get_sessions(user_id):
     response = make_response(jsonify(theData))
     response.status_code = 200
     return response
+
+#------------------------------------------------------------
+# Create a new session
+@sessions.route('/sessions', methods=['POST'])
+def create_session(mentor_id, mentee_id, session_date, session_time):
+    """
+    This route is used by mentors/mentees to create a new session.
+    """
+
+    query = f'''insert into session (mentor_id, mentee_id, session_date, session_time)
+                values ({str(mentor_id)}, {str(mentee_id)}, {str(session_date)}, {str(session_time)})
+    '''
+
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+
+    response = make_response(jsonify({"message": "Session created successfully!!"}))
+    response.status_code = 200
+    return response
+
