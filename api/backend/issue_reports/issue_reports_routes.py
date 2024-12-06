9db08426cd76a859be641fa0344cc9c0c9f580b3
rxie <89948424+shdwfruit@users.noreply.github.com>
2024-12-05 21:33:40 -0500

    Update issue_reports_routes.py
    added issue report route

--- api/backend/issue_reports/issue_reports_routes.py
+++ api/backend/issue_reports/issue_reports_routes.py
@@ -12,3 +12,20 @@ issue_reports = Blueprint('issue_reports', __name__)
 
 #------------------------------------------------------------
 
+@issue_reports.route('/get_reports', methods=['GET'])
+def fetch_reported_issues():
+
+    query = '''
+            SELECT ir.id, ir.description, ir.status, ir.timestamp, 
+                    sa.first_name AS resolved_by
+            FROM issue_report ir
+            LEFT JOIN system_administrator sa ON ir.resolved_by = sa.id
+            WHERE ir.status = 'Open';
+    '''
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    theData = cursor.fetchall()
+
+    response = make_response(jsonify(theData))
+    response.status_code = 200
+    return response
